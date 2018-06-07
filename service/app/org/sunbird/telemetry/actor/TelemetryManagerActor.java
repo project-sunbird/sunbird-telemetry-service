c2eceaa419619b9af70a1abc6172012d47aad34d
Vinaya Kumar B <vinayab@ilimi.in>
2018-06-07 23:13:44 +0530

    Issue #SB-0000 fix: For troubleshooting high cpu usage

--- service/app/org/sunbird/telemetry/actor/TelemetryManagerActor.java
+++ service/app/org/sunbird/telemetry/actor/TelemetryManagerActor.java
@@ -4,7 +4,6 @@ import akka.actor.ActorRef;
 import com.typesafe.config.Config;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import org.apache.commons.lang3.StringUtils;
 import org.sunbird.actor.core.BaseActor;
 import org.sunbird.actor.router.ActorConfig;
@@ -14,7 +13,6 @@ import org.sunbird.common.models.util.JsonKey;
 import org.sunbird.common.models.util.LoggerEnum;
 import org.sunbird.common.models.util.ProjectLogger;
 import org.sunbird.common.request.Request;
-import org.sunbird.telemetry.dispatcher.EkstepTelemetryDispatcher;
 import org.sunbird.util.ConfigUtil;
 import util.Constant;
 import util.EnvConstant;
@@ -56,32 +54,38 @@ public class TelemetryManagerActor extends BaseActor {
   @SuppressWarnings("unchecked")
   @Override
   public void onReceive(Request request) throws Throwable {
-    String operation = request.getOperation();
-    String ekstepStorageToggle = config.getString(EnvConstant.EKSTEP_TELEMETRY_STORAGE_TOGGLE);
-    if (Constant.DISPATCH_TELEMETRY_OPERATION_NAME.equals(operation)) {
-      Object body = request.get(JsonKey.BODY);
-      Map<String, String[]> headers = (Map<String, String[]>) request.get(Constant.HEADERS);
-      // if ekstep_telemetry_storage_toggle is on then only send the telemetry value
-      // to ekstep.
-      if (StringUtils.equalsIgnoreCase(Constant.ON, ekstepStorageToggle)) {
-        if (body instanceof String) {
-          EkstepTelemetryDispatcher.dispatch(headers, (String) body);
-        } else if (body instanceof byte[]) {
-          EkstepTelemetryDispatcher.dispatch(headers, (byte[]) body);
-        } else {
-          onReceiveUnsupportedMessage(operation);
-        }
-      }
-      Response response = new Response();
-      response.put(JsonKey.RESPONSE, JsonKey.SUCCESS);
-      sender().tell(response, self());
+    Response response = new Response();
+    response.put(JsonKey.RESPONSE, JsonKey.SUCCESS);
+    sender().tell(response, self());
 
-      for (String name : dispatchers) {
-        dispatch(name, request);
-      }
-    } else {
-      onReceiveUnsupportedOperation(operation);
-    }
+    /*
+        String operation = request.getOperation();
+        String ekstepStorageToggle = config.getString(EnvConstant.EKSTEP_TELEMETRY_STORAGE_TOGGLE);
+        if (Constant.DISPATCH_TELEMETRY_OPERATION_NAME.equals(operation)) {
+          Object body = request.get(JsonKey.BODY);
+          Map<String, String[]> headers = (Map<String, String[]>) request.get(Constant.HEADERS);
+          // if ekstep_telemetry_storage_toggle is on then only send the telemetry value
+          // to ekstep.
+          if (StringUtils.equalsIgnoreCase(Constant.ON, ekstepStorageToggle)) {
+            if (body instanceof String) {
+              EkstepTelemetryDispatcher.dispatch(headers, (String) body);
+            } else if (body instanceof byte[]) {
+              EkstepTelemetryDispatcher.dispatch(headers, (byte[]) body);
+            } else {
+              onReceiveUnsupportedMessage(operation);
+            }
+          }
+          Response response = new Response();
+          response.put(JsonKey.RESPONSE, JsonKey.SUCCESS);
+          sender().tell(response, self());
+
+          for (String name : dispatchers) {
+            dispatch(name, request);
+          }
+        } else {
+          onReceiveUnsupportedOperation(operation);
+        }
+    */
   }
 
   private void getDispatchers() {
