a5ac259db936a21745ea594265c53295f75c3ecd
Vinaya Kumar B <vinayab@ilimi.in>
2018-06-08 15:30:05 +0530

    Issue #SB-4324 fix: For troubleshooting high cpu usage

--- service/app/org/sunbird/telemetry/dispatcher/KafkaTelemetryDispatcherActor.java
+++ service/app/org/sunbird/telemetry/dispatcher/KafkaTelemetryDispatcherActor.java
@@ -17,6 +17,7 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.sunbird.actor.core.BaseActor;
 import org.sunbird.actor.router.ActorConfig;
 import org.sunbird.common.exception.ProjectCommonException;
+import org.sunbird.common.models.response.Response;
 import org.sunbird.common.models.util.JsonKey;
 import org.sunbird.common.models.util.LoggerEnum;
 import org.sunbird.common.models.util.ProjectLogger;
@@ -54,6 +55,9 @@ public class KafkaTelemetryDispatcherActor extends BaseActor {
     String operation = request.getOperation();
     if (Constant.DISPATCH_TELEMETRY_TO_KAFKA.equals(operation)) {
       List<String> events = getEvents(request);
+      Response response = new Response();
+      response.put(JsonKey.RESPONSE, JsonKey.SUCCESS);
+      sender().tell(response, self());
       dispatchEvents(events);
     } else {
       onReceiveUnsupportedMessage(operation);
