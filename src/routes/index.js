0ec3cea9a8cc7a9a3ee12184d104701b20df9797
Santhosh <santhosh@ilimi.in>
2018-06-28 16:36:44 +0530

    Issue #000 feat: enhancements
    1. Proxy telemetry data to a given proxy url enabled via configuration
    2. cassanda ttl

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,7 +6,7 @@ router.post('/v1/telemetry', function(req, res, next) {
   req.body.did = req.header('x-device-id');
   req.body.channel = req.header('x-channel-id');
   req.body.pid = req.header('x-app-id');
-  telemetryService.dispatch(req.body, res);
+  telemetryService.dispatch(req, res);
 })
 
 router.get('/health', function(req, res, next) {
--- src/service/telemetry-service.js
+++ src/service/telemetry-service.js
@@ -1,8 +1,10 @@
 var uuidv1 = require('uuid/v1');
+var request = require('request');
 var Dispatcher = require('../dispatcher/dispatcher').Dispatcher;
 
 const localStorageEnabled = process.env.telemetry_local_storage_enabled || 'true';
-const proxyTelemetryData = process.env.telemetry_proxy_pass_through;
+const telemetryProxyEnabled = process.env.telemetry_proxy_enabled;
+const proxyURL = process.env.telemetry_proxy_url;
 
 // TODO: Make this efficient. Implementation to be similar to typesafe config. Right now one configuration holds 
 // together all supported transport configurations
@@ -21,7 +23,8 @@ const config = {
     maxFiles: process.env.telemetry_file_maxfiles || '100',
     partitionBy: process.env.telemetry_cassandra_partition_by || 'hour',
     keyspace: process.env.telemetry_cassandra_keyspace,
-    contactPoints: (process.env.telemetry_cassandra_contactpoints || 'localhost').split(',')
+    contactPoints: (process.env.telemetry_cassandra_contactpoints || 'localhost').split(','),
+    cassandraTtl: process.env.telemetry_cassandra_ttl
 }
 
 var dispatcher = undefined;
@@ -30,20 +33,55 @@ if(localStorageEnabled === 'true') {
     dispatcher = new Dispatcher(config);
 }
 
-exports.dispatch = function(message, res) {
+const SUCCESS_RESP = {id: 'api.telemetry', ver: '1.0', ets: new Date().getTime(), params: {}, responseCode: "SUCCESS"};
+const ERROR_RESP = {id: 'api.telemetry', ver: '1.0', ets: new Date().getTime(), params: {err: err}, responseCode: "SERVER_ERROR"};
+
+exports.dispatch = function(req, res) {
+    
+    var message = req.body;
     if(!message.mid) message.mid = uuidv1();
     message.syncts = new Date().getTime();
     const data = JSON.stringify(message);
-    if(localStorageEnabled === 'true') {
-        dispatcher.dispatch(message.mid, data, function(err, data) {
-            if(err) {
-                console.log('error', err);
-                res.status(500)
-                res.json({id: 'api.telemetry', ver: '1.0', ets: new Date().getTime(), params: {err: err}, responseCode: "SERVER_ERROR"});
-            } else {
-                res.json({id: 'api.telemetry', ver: '1.0', ets: new Date().getTime(), params: {}, responseCode: "SUCCESS"});
-            }
-        });
+    const headers = {'content-type': req.header('content-type'), 'content-encoding': req.header('content-encoding')};
+    
+    if(localStorageEnabled === 'true' || telemetryProxyEnabled === 'true') {
+
+        if(localStorageEnabled === 'true') {
+            // Store locally and respond back with proper status code
+            dispatcher.dispatch(message.mid, data, function(err, data) {
+                if(err) {
+                    console.log('error', err);
+                    res.status(500)
+                    res.json(ERROR_RESP);
+                } else {
+                    res.json(SUCCESS_RESP);
+                }
+            });
+        }
+        if(localStorageEnabled === 'true' && telemetryProxyEnabled === 'true') {
+            // Store locally and proxy to the specified URL. If the proxy fails ignore the error as the local storage is successful. Do a sync later
+            var options = {url:proxyURL, headers: headers, body: data};
+            request.post(options, function optionalCallback(err, httpResponse, body) {
+                if (err) {
+                    return console.error('Proxy failed:', err);
+                }
+                console.log('Proxy successful!  Server responded with:', body);
+            });
+        } 
+        if(localStorageEnabled !== 'true' && telemetryProxyEnabled === 'true') {
+            
+            // Just proxy
+            var options = {url:proxyURL, headers: headers, body: data};
+            request.post(options, function optionalCallback(err, httpResponse, body) {
+                if (err) {
+                    res.status(500)
+                    res.json(ERROR_RESP);
+                } else {
+                    console.log('Proxy successful!  Server responded with:', body);
+                    res.json(SUCCESS_RESP);
+                }
+            });
+        }
     } else {
         res.status(500)
         res.json({id: 'api.telemetry', ver: '1.0', ets: new Date().getTime(), params: {err: 'Configuration error.'}, responseCode: "SERVER_ERROR"});
