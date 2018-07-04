1eb4c4f1acbe9e99728634dedcff266766076812
Mahesh Kumar Gangula <mahesh@ilimi.in>
2018-07-04 17:17:10 +0530

    Issue #000 fix: added datatype in CORS headers.

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ cluster(function(worker) {
   	res.header('Access-Control-Allow-Origin', '*')
   	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE,OPTIONS')
   	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization,' +
-                                              'cid, user-id, x-auth, Cache-Control, X-Requested-With, *')
+                                              'cid, user-id, x-auth, Cache-Control, X-Requested-With, datatype, *')
 
   	if (req.method === 'OPTIONS') {
     	res.sendStatus(200)
