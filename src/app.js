ede1931595fac14a4ef9eb510db47ee9a6e3cf14
Harishkumar Gangula <harishg@ilimi.in>
2019-02-27 14:09:04 +0530

    Issue #000 fix: adding limit to urlencoded

--- src/app.js
+++ src/app.js
@@ -7,31 +7,31 @@ const express = require('express'),
   port = envVariables.port,
   threads = envVariables.threads;
 
-  const createAppServer = () => {
-    const app = express();
-    app.use((req, res, next) => {
-      res.header('Access-Control-Allow-Origin', '*')
-      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE,OPTIONS')
-      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization,' + 'cid, user-id, x-auth, Cache-Control, X-Requested-With, datatype, *')
-      if (req.method === 'OPTIONS') res.sendStatus(200)
-      else next()
-    })
-    app.use(bodyParser.json({ limit: '1mb' }));
-    app.use(logger('dev'));
-    app.use(express.json());
-    app.use(bodyParser.urlencoded({ extended: false }));
-    app.use(cookieParser());
-    app.use('/', require('./routes'));
-    module.exports = app;
-    return app;
-  }
+const createAppServer = () => {
+  const app = express();
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE,OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization,' + 'cid, user-id, x-auth, Cache-Control, X-Requested-With, datatype, *')
+    if (req.method === 'OPTIONS') res.sendStatus(200)
+    else next()
+  })
+  app.use(bodyParser.json({ limit: '1mb' }));
+  app.use(logger('dev'));
+  app.use(express.json());
+  app.use(bodyParser.urlencoded({ limit: '1mb', extended: false }));
+  app.use(cookieParser());
+  app.use('/', require('./routes'));
+  module.exports = app;
+  return app;
+}
 
-  if (process.env.node_env !== 'test') {
-    cluster((worker) => {
-      const app = createAppServer();
-      return app.listen(port, () => console.log(`telemetry services cluster is running on port ${port} with ${process.pid} pid`));
-    }, { count: threads });
-  } else {
+if (process.env.node_env !== 'test') {
+  cluster((worker) => {
     const app = createAppServer();
-    app.listen(port, () => console.log(`telemetry services is running in test env on port ${port} with ${process.pid} pid`));
-  }
+    return app.listen(port, () => console.log(`telemetry services cluster is running on port ${port} with ${process.pid} pid`));
+  }, { count: threads });
+} else {
+  const app = createAppServer();
+  app.listen(port, () => console.log(`telemetry services is running in test env on port ${port} with ${process.pid} pid`));
+}
