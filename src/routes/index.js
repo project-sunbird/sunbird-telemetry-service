1c114434ab42247091c1c11017a84815ce4e0623
Sourav Dey <sourav_d@techjoomla.com>
2019-03-13 10:59:38 +0530

    Issue #SB-9770 feat: Adding new health check API for telemetry service

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,4 +6,6 @@ router.post('/v1/telemetry', (req, res) => telemetryService.dispatch(req, res));
 
 router.get('/health', (req, res) => telemetryService.health(req, res));
 
+router.get('/service/health', (req, res) => telemetryService.telemetryServiceHealth(req, res));
+
 module.exports = router;
