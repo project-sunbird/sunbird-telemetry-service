83da2462b3f7ed6ff70097c19916a9b3a5a7969a
Deeksha Deepak <deekshad1502@gmail.com>
2025-05-07 18:57:20 +0530

    SBCOSS-407: Added eslint for envVariables file

--- src/envVariables.js
+++ src/envVariables.js
@@ -1,24 +1,24 @@
 const os = require('os');
 
 const envVariables = {
-    level: process.env.telemetry_log_level || 'info',
-    localStorageEnabled: process.env.telemetry_local_storage_enabled || 'true',
-    telemetryProxyEnabled: process.env.telemetry_proxy_enabled,
-    dispatcher: process.env.telemetry_local_storage_type,
-    proxyURL: process.env.telemetry_proxy_url,
-    proxyAuthKey: process.env.telemetry_proxy_auth_key,
-    encodingType: process.env.telemetry_encoding_type,
-    kafkaHost: process.env.telemetry_kafka_broker_list,
-    topic: process.env.telemetry_kafka_topic,
-    compression_type: process.env.telemetry_kafka_compression || 'none',
-    filename: process.env.telemetry_file_filename || 'telemetry-%DATE%.log',
-    maxSize: process.env.telemetry_file_maxsize || '100m',
-    maxFiles: process.env.telemetry_file_maxfiles || '100',
-    partitionBy: process.env.telemetry_cassandra_partition_by || 'hour',
-    keyspace: process.env.telemetry_cassandra_keyspace,
-    contactPoints: (process.env.telemetry_cassandra_contactpoints || 'localhost').split(','),
-    cassandraTtl: process.env.telemetry_cassandra_ttl,
-    port: process.env.telemetry_service_port || 9001,
-    threads: process.env.telemetry_service_threads || os.cpus().length
-}
+  level: process.env.telemetry_log_level || 'info',
+  localStorageEnabled: process.env.telemetry_local_storage_enabled || 'true',
+  telemetryProxyEnabled: process.env.telemetry_proxy_enabled,
+  dispatcher: process.env.telemetry_local_storage_type,
+  proxyURL: process.env.telemetry_proxy_url,
+  proxyAuthKey: process.env.telemetry_proxy_auth_key,
+  encodingType: process.env.telemetry_encoding_type,
+  kafkaHost: process.env.telemetry_kafka_broker_list,
+  topic: process.env.telemetry_kafka_topic,
+  compression_type: process.env.telemetry_kafka_compression || 'none',
+  filename: process.env.telemetry_file_filename || 'telemetry-%DATE%.log',
+  maxSize: process.env.telemetry_file_maxsize || '100m',
+  maxFiles: process.env.telemetry_file_maxfiles || '100',
+  partitionBy: process.env.telemetry_cassandra_partition_by || 'hour',
+  keyspace: process.env.telemetry_cassandra_keyspace,
+  contactPoints: (process.env.telemetry_cassandra_contactpoints || 'localhost').split(','),
+  cassandraTtl: process.env.telemetry_cassandra_ttl,
+  port: process.env.telemetry_service_port || 9001,
+  threads: process.env.telemetry_service_threads || os.cpus().length
+};
 module.exports = envVariables;
